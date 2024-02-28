b8cf4d5b1fe334210d6de89f4facdf9591a1e011
Ronald cristian <ronaldcristian1212@gmail.com>
2024-02-28 00:01:01 -0300

    Create Scatter_plot.R
    cria uma visualização de dispersão (scatter plot) para explorar a relação entre duas variáveis

---
+++ R/Exercícios/Scatter_plot.R
@@ -0,0 +1,7 @@
+# Scatter plot
+plot_dispersao <- function(x, y) {
+  plot(x, y, main = "Dispersão entre variável X e variável Y", xlab = "X", ylab = "Y")
+}
+
+# Exemplo de uso:
+# plot_dispersao(dados$variavel_x, dados$variavel_y)
